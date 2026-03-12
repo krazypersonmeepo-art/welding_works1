10fe74a261dcabff1329315dd8bdb56c782cd3f9
kristanguanzon <krazypersonmeepo@gmail.com>
2026-03-12 16:41:22 +0800

    Add Netlify build config

---
+++ scripts/netlify-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+FLUTTER_DIR="$HOME/flutter"
+
+if [ ! -d "$FLUTTER_DIR" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1 "$FLUTTER_DIR"
+fi
+
+export PATH="$FLUTTER_DIR/bin:$PATH"
+
+flutter --version
+flutter pub get
+flutter build web --release
+
